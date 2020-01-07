67821d9a4adf055030b0924838b9b61c0adce909
KJ Magill <39092621+kjmagill@users.noreply.github.com>
2020-01-06 19:11:26 -0500

    Login form & validation complete - just needs styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,44 +1,70 @@
-import React, { PureComponent } from 'react';
-import { Form, Field, ErrorMessage, withFormik } from 'formik';
-import * as yup from 'yup';
+import React from 'react';
+import { Formik } from 'formik';
 
-export default class Login extends PureComponent {
-    render() {
-        return (
-            <Form>
-                <Field type="text" name="email" placeholder="email" />
-                <ErrorMessage name="email" />
-                <Field type="text" name="password" placeholder="password" />
-                <ErrorMessage name="password" />
-                <button type="submit"> Submit </button>
-            </Form>
-        )
-    }
-}
+const Login = () => (
+    <div className="login-form" style={{ textAlign: "center" }}>
+        <h1 style={{ fontSize: "3rem" }}>Login:</h1>
+        <Formik
+            initialValues={{ email: '', password: '' }}
+            validate={values => {
+                const errors = {};
+                if (!values.email) {
+                    errors.email = 'Required';
+                } else if (
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                ) {
+                    errors.email = 'Invalid email address';
+                }
+                return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                }, 400);
+            }}
+        >
+            {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+            }) => (
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="email"
+                        name="email"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                        placeholder="Email"
+                        style={{ width: "300px", height: "30px", fontSize: "1.2rem", paddingLeft: "6px", marginRight: "2px" }}
+                    />
+                    {errors.email && touched.email && errors.email}
+                    <input
+                        type="password"
+                        name="password"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.password}
+                        placeholder="Password"
+                        style={{ width: "300px", height: "30px", fontSize: "1.2rem", paddingLeft: "6px", marginLeft: "2px" }}
+                    />
+                    {errors.password && touched.password && errors.password}
+                    <button
+                        type="submit"
+                        disabled={isSubmitting}
+                        style={{ width: "80px", height: "35px", fontSize: "1.2rem", marginLeft: "4px" }}
+                    >
+                        Submit
+                    </button>
+                </form>
+            )}
+        </Formik>
+    </div>
+);
 
-const LoginWrapper = Login
-
-const LoginValidation = yup.object().shape({
-    email: yup
-        .string()
-        .email()
-        .required(),
-    password: yup
-        .string()
-        .min(8)
-        .max(16)
-        .matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*d)[a-zA-Zd]$")
-        .required(),
-})
-
-withFormik({
-    // Handles our submission
-    handleSubmit: (values, { setSubmitting }) => {
-    // This is where you could send the submitted values to the backend
-    console.log("Submitted Email:", values.email)
-    console.log("Submitted Password:", values.password)
-    // Simulates the delay of a real request
-    setTimeout(() => setSubmitting(false), 3 * 1000)
-    },
-    validationSchema: LoginValidation,
-})(LoginWrapper)
+export default Login;
