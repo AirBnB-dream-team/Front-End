c28c6683818aab7c71bd6c8c565324e274b293b0
Prem Puttegowda <puttegowdaprem@gmail.com>
2020-01-06 20:10:39 -0800

    moved registration form to components folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 import Login from './components/Login';
-import RegisterForm from './RegisterForm'
+import RegisterForm from './components/RegisterForm';
 
 function App() {
   return (
