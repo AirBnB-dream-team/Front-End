4e213bb7cddadf8c719abd7ecfea1aa61a05c3b1
KJ Magill <39092621+kjmagill@users.noreply.github.com>
2020-01-08 13:24:11 -0500

    Added span tags to MyListingsCard.js for each line within the cards.

--- src/components/MyListingsCards.js
+++ src/components/MyListingsCards.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
 
-
 const MyListingsCards = props => {
 
     return (
         <div className="mylistings">
             <h1>{props.listing.address}</h1>
-            <h3>{props.listing.city}, {props.listing.state} {props.listing.zip}</h3>
-            <p>Price:</p> <h4>{props.listing.price}</h4>
-            <p>Baths:</p> <h4>{props.listing.bath_number}</h4>
-            <p>Beds:</p> <h4>{props.listing.bed_number}</h4>
-            <p>Sq.Ft:</p> <h4>{props.listing.sqft}</h4>
-            <p>Date:</p> <h4>{props.listing.date}</h4>
-            <p>Email:</p> <h4>{props.listing.email}</h4>
-            
+            <span><h3>{props.listing.city}, {props.listing.state} {props.listing.zip}</h3></span>
+            <span><p>Price:</p> <h4>{props.listing.price}</h4></span>
+            <span><p>Baths:</p> <h4>{props.listing.bath_number}</h4></span>
+            <span><p>Beds:</p> <h4>{props.listing.bed_number}</h4></span>
+            <span><p>Sq.Ft:</p> <h4>{props.listing.sqft}</h4></span>
+            <span><p>Date:</p> <h4>{props.listing.date}</h4></span>
+            <span><p>Email:</p> <h4>{props.listing.email}</h4></span>
         </div>
     );
 };
